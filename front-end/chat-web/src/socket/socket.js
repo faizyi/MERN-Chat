64240edde2a674d96bbf6fa9e187d22d867d6fc6
faizyi <faizi13062000@gmail.com>
2024-08-29 00:59:44 +0500

    vercel

--- front-end/chat-web/src/socket/socket.js
+++ front-end/chat-web/src/socket/socket.js
@@ -2,5 +2,6 @@ import io from "socket.io-client"
 
 const socket = io("https://chat-backend-tau-two.vercel.app",{
     withCredentials: true,
+    transports: ["websocket", "polling"], 
 });
 export default socket;
