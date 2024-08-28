ba219e45aca2776ad79b8185edad2f1576511039
faizyi <faizi13062000@gmail.com>
2024-08-29 00:49:41 +0500

    set url

--- front-end/chat-web/src/socket/socket.js
+++ front-end/chat-web/src/socket/socket.js
@@ -1,6 +1,6 @@
 import io from "socket.io-client"
 
-const socket = io("http://localhost:7777",{
+const socket = io("https://chat-backend-tau-two.vercel.app",{
     withCredentials: true,
 });
 export default socket;
