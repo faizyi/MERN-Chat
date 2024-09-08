8f758e71230522a8440abc039510b72dfd682712
faizyi <faizi13062000@gmail.com>
2024-09-08 15:20:52 +0500

    url

--- front-end/chat-web/src/axios/axios.js
+++ front-end/chat-web/src/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 // const url = "https://chat-backend-tau-two.vercel.app/"
 export const axiosHandler = axios.create({
-    baseURL : "http://localhost:7777/",
+    baseURL : "https://chat-backend-tau-two.vercel.app/",
     headers : {"Content-Type" : "application/json"},
     withCredentials : true
 })
